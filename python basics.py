a19907fb5143ed31df8439dfeb5cf2442e4e0bec
Sharika Loganathan <37877198+sharikalog7@users.noreply.github.com>
2020-06-08 14:52:10 +0530

    Add files via upload

---
+++ python basics.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("hello world ")
+
+
+# In[4]:
+
+
+3+5,2+5,10*2,10/2
+
+
+# In[5]:
+
+
+"string"*3
+
+
+# In[7]:
+
+
+3<5,10<4
+
+
+# In[12]:
+
+
+3+5 and 4+5,3+5 or 4+5 
+
+
+# In[16]:
+
+
+3 and 5  , 3 or 5
+
+
+# In[17]:
+
+
+3 >5  or 6<10
+
+
+# In[18]:
+
+
+3==5 or 5==5
+
+
+# In[26]:
+
+
+3 <5 and 5>3 , 3<5 or 5 < 3
+
+
+# In[25]:
+
+
+not 5 , not -8 , not "string" 
+
+
+# In[27]:
+
+
+a=1
+not a 
+
+
+# In[29]:
+
+
+not 0
+
+
+# In[30]:
+
+
+35// 3.0
+
+
+# In[31]:
+
+
+35/ 9
+
+
+# In[32]:
+
+
+neeraj+20
+
+
+# In[33]:
+
+
+"abc"+"5"
+
+
+# In[49]:
+
+
+10 >5 , 5>10 , 30 !=70 , 40==40  
+
+
+# In[50]:
+
+
+2* 2 > 3*3
+
+
+# In[51]:
+
+
+0 and 5
+
+#chekcs if first is false 
+#0 is false so it returns 0
+
+
+# In[ ]:
+
+
+
+
+
+# In[52]:
+
+
+3 or 0
+
+#here 3 is not false and returns 3 
+
+
+# In[53]:
+
+
+3 and 5 
+   
+  # first is  not false moves to second
+
+
+# In[54]:
+
+
+0  or 5 
+
+#checks first true value  it returns first one otherwise returns last one
+
+
+# In[55]:
+
+
+5 or 0
+
+
+# In[56]:
+
+
+5 or 3
+
+
+# In[ ]:
+
+
+
+
